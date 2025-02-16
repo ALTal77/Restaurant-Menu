d7f2ae643d479f20b36ca3b70785284e821d6f7f
ALTal77 <ahmadtall7@hotmail.com>
2025-02-16 20:49:39 +0300

    Delete re.js

--- re.js
+++
@@ -1,8 +0,0 @@
-let navLinks = document.querySelectorAll('.nav-link');
-let navbarCollapse = document.querySelector('.navbar-collapse');
-
-navLinks.forEach(function(link) {
-      link.addEventListener('click', function() {
-        navbarCollapse.classList.remove('show');
-});
-});
